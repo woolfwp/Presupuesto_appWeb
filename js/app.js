6b8f3d53dc7a3055295c011dfaeb34209cd61b3a
Woolfwp <sebasgomez1099@gmail.com>
2021-08-03 11:53:17 -0500

    Agregando Js

---
+++ js/app.js
@@ -0,0 +1,17 @@
+const ingresos = [
+  new Ingreso('Salario', 2100),
+  new Ingreso('venta coche',1000)  
+];
+
+const egresos = [
+    new Egreso('pago tarjeta', 100),
+    new Egreso('comida', 300)
+];
+
+let cargarApp = () =>{
+    cargarCabecero();
+}
+
+let cargarCabecero= () =>{
+    
+}
